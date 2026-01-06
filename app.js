d4fa7c802021f6f3410ae4654a25672ca39d1ee5
8061-maker <8061@crms5denha.ac.th>
2026-01-06 22:07:18 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//let age = prompt("อายุเท่าไหร่จ๊ะ")
+//if(age > 12 ){
+    document.getElementById("content").innerHTML = "<img data-src=\"https://dl.dir.freefiremobile.com/common/web_event/official2.ff.garena.all/202210/82fe305f7dbb9f25e68996f8f719e576.jpg\" src=\"https://dl.dir.freefiremobile.com/common/web_event/official2.ff.garena.all/202210/82fe305f7dbb9f25e68996f8f719e576.jpg\" alt=\"wallpaper-img\" class=\"wallpaper-li-img lazy-image lazy-image-active\" data-v-a74d4924=\"\">"
+//}
+document.getElementById("content").innerHTML = "<img data-src=\"https://dl.dir.freefiremobile.com/common/web_event/official2.ff.garena.all/202210/82fe305f7dbb9f25e68996f8f719e576.jpg\" src=\"https://dl.dir.freefiremobile.com/common/web_event/official2.ff.garena.all/202210/82fe305f7dbb9f25e68996f8f719e576.jpg\" alt=\"wallpaper-img\" class=\"wallpaper-li-img lazy-image lazy-image-active\" data-v-a74d4924=\"\">"
+
+
+document.getElementById("freefire").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/f_gN2-_UZIs?si=plkSRr0_eo1aCfWm\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" \allowfullscreen></iframe\>"
